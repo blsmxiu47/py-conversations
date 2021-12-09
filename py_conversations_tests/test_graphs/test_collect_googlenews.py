2065b03f993e5dd6f09bd77076d51de782c3eed2
weswa <fridgehistory@gmail.com>
2021-12-08 16:01:54 -0800

    add initial ops and graphs testing files for googlenews pipeline

---
+++ py_conversations_tests/test_graphs/test_collect_googlenews.py
@@ -0,0 +1,11 @@
+from py_conversations.graphs.collect_googlenews import collect_googlenews
+
+
+def test_collect_googlenews():
+    """
+    TODO: this.*
+    """
+    result = collect_googlenews.execute_in_process()
+
+    assert result.success
+    assert result.output_for_node({}) == 200
---
+++ py_conversations_tests/test_ops/test_ingest_data.py
@@ -0,0 +1,9 @@
+from py_conversations.ops.ingest_data import ingest_data
+
+
+def test_ingest_data():
+    """
+    TODO: this.*
+    """
+
+    assert ingest_data({}) == True
