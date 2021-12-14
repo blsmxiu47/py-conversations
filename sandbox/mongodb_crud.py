0db513539be73f8ca73964f6d4f9081acea623d3
Wes Warriner <fridgehistory@gmail.com>
2021-12-14 11:43:48 -0800

    add some more testing to pymongo sandbox

--- sandbox/mongodb_crud.py
+++ sandbox/mongodb_crud.py
@@ -1,5 +1,9 @@
+import logging
+
 from pymongo import MongoClient
 from pprint import pprint
+
+from pymongo.errors import BulkWriteError
 
 import settings
 
@@ -11,6 +15,35 @@ MONGO_DB_NAME = settings.MONGO_DB_NAME
 client = MongoClient(
     f'mongodb+srv://{MONGO_USERNAME}:{MONGO_PW}@{MONGO_CLUSTER_NAME}.seck8.mongodb.net/{MONGO_DB_NAME}?retryWrites=true&w=majority'
 )
-db = client.admin
-serverStatusResult = db.command("serverStatus")
-pprint(serverStatusResult)
+
+db_list = client.list_database_names()
+pprint(db_list)
+
+new_db = client['test_db']
+new_coll = new_db['test_collection']
+
+test_items = [
+    {"item": "journal",
+     "qty": 25,
+     "tags": ["blank", "red"],
+     "size": {"h": 14, "w": 21, "uom": "cm"}},
+    {"item": "mat",
+     "qty": 85,
+     "tags": ["gray"],
+     "size": {"h": 27.9, "w": 35.5, "uom": "cm"}},
+    {"item": "mousepad",
+     "qty": 25,
+     "tags": ["gel", "blue"],
+     "size": {"h": 19, "w": 22.85, "uom": "cm"}}]
+try:
+    result = new_coll.insert_many(test_items)
+except BulkWriteError as e:
+    logging.error(e)
+print('Result acknowledged?: ', result.acknowledged)
+
+coll_list = new_db.list_collection_names()
+pprint(coll_list)
+db = client['test_db']
+print(db)
+# serverStatusResult = db.command('serverStatus')
+# pprint(serverStatusResult)
