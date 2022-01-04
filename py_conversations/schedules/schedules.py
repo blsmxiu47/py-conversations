50b7c10aacc75885e860e29918ffa0e261b34e2f
Wes Warriner <fridgehistory@gmail.com>
2022-01-04 15:03:11 -0800

    update schedule with MONGO_ environment variables

--- py_conversations/schedules/schedules.py
+++ py_conversations/schedules/schedules.py
@@ -4,9 +4,16 @@ from dagster import schedule
 from dagster.core.definitions import RunRequest
 
 from py_conversations.graphs.collect_insert_googlenews import collect_insert_googlenews
+import settings
 
+env_vars = {
+    'MONGO_USERNAME': settings.MONGO_USERNAME,
+    'MONGO_PW': settings.MONGO_PW,
+    'MONGO_CLUSTER_NAME': settings.MONGO_CLUSTER_NAME,
+    'MONGO_DB_NAME': settings.MONGO_DB_NAME
+}
 
-@schedule(cron_schedule='0 * * * *', job=collect_insert_googlenews, execution_timezone='US/Pacific')
+@schedule(cron_schedule='0 * * * *', job=collect_insert_googlenews, environment_vars=env_vars, execution_timezone='US/Pacific')
 def hourly_collect_googlenews(_context):
     now = datetime.now()
-    return RunRequest(run_key=now.strftime('%m/%d/%Y, %H:%M:%S'))
+    return RunRequest(run_key=now.strftime('%m/%d/%Y %H:%M:%S'))
