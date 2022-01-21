0c97c2f48665b09d95a84f747db23e4231861957
Wes Warriner <fridgehistory@gmail.com>
2022-01-21 10:27:33 -0800

    add temporary default parameters and TODO for tweets graph

--- py_conversations/graphs/collect_insert_tweets.py
+++ py_conversations/graphs/collect_insert_tweets.py
@@ -5,7 +5,9 @@ from py_conversations.ops.get_tweets import get_tweets
 
 @graph
 def collect_insert_tweets():
-    insert_items(get_tweets())
+    topic = 'wildfires' # tmp, this'll actually be coming from the dashboard user's settings or selection
+    last_id = None # tmp
+    insert_items(get_tweets(topic, last_id)[0]) # TODO: insert_items takes a list, but we also need to In the last id from the last run and store the last id from this run
 
 
 collect_insert_tweets = collect_insert_tweets.to_job()
