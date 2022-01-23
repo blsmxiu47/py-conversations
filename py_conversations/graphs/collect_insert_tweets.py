839fd287cd1811b4fa13ccc803f4ece00f011866
Wes Warriner <fridgehistory@gmail.com>
2022-01-23 11:15:34 -0800

    update collect_insert_tweets graph decorator with InputDefinitions

--- py_conversations/graphs/collect_insert_tweets.py
+++ py_conversations/graphs/collect_insert_tweets.py
@@ -1,13 +1,15 @@
-from dagster import graph
+from dagster import InputDefinition, graph
 from py_conversations.ops.insert_items import insert_items
 from py_conversations.ops.get_tweets import get_tweets
 
 
-@graph
-def collect_insert_tweets():
-    topic = 'wildfires' # tmp, this'll actually be coming from the dashboard user's settings or selection
-    last_id = None # tmp
-    insert_items(get_tweets(topic, last_id)[0]) # TODO: insert_items takes a list, but we also need to In the last id from the last run and store the last id from this run
-
-
-collect_insert_tweets = collect_insert_tweets.to_job()
+@graph(
+    name='collect_insert_tweets',
+    input_defs=[
+        InputDefinition('topic', str, default_value='wildfires'),
+        InputDefinition('last_id', int, default_value=1485314390639333378),
+        InputDefinition('limit', int, default_value=1)
+    ]
+)
+def collect_insert_tweets(topic='wildfires', last_id=1485314390639333378, limit=1):
+    insert_items(get_tweets(topic, last_id, limit)[0]) # TODO: insert_items takes a list, but we also need to In the last id from the last run and store the last id from this run
