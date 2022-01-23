a240971e75220d8d8137ef8aa61cffbf4277b18f
Wes Warriner <fridgehistory@gmail.com>
2022-01-23 10:14:22 -0800

    update get_tweets test to use all current parameters

--- py_conversations_tests/test_ops/test_get_tweets.py
+++ py_conversations_tests/test_ops/test_get_tweets.py
@@ -1,7 +1,7 @@
 from py_conversations.ops.get_tweets import get_tweets
 
 def test_get_tweets():
-    response = get_tweets('wildfires')
+    response = get_tweets(topic='wildfires', last_id=1485314390639333378, limit=2)
     assert isinstance(response, tuple), "returned object should be of type 'tuple'"
     assert isinstance(response[0], list), "first tuple item should be of type 'list'"
     assert isinstance(response[0][0], dict), "tweets list items should be of type 'dict'"
