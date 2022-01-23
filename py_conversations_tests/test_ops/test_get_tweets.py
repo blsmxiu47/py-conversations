f79535e36e83d224d048022b308744ebee1406fa
Wes Warriner <fridgehistory@gmail.com>
2022-01-23 10:10:05 -0800

    add test for get_tweets op

---
+++ py_conversations_tests/test_ops/test_get_tweets.py
@@ -0,0 +1,8 @@
+from py_conversations.ops.get_tweets import get_tweets
+
+def test_get_tweets():
+    response = get_tweets('wildfires')
+    assert isinstance(response, tuple), "returned object should be of type 'tuple'"
+    assert isinstance(response[0], list), "first tuple item should be of type 'list'"
+    assert isinstance(response[0][0], dict), "tweets list items should be of type 'dict'"
+    assert isinstance(response[1], int), "second tuple item should be of type 'int'"
