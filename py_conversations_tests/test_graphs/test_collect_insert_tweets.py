36fb3d158071773f0de588eed3aa34179b3a1b62
Wes Warriner <fridgehistory@gmail.com>
2022-01-29 09:25:06 -0800

    add docstring for test of collect_insert_tweets graph

--- py_conversations_tests/test_graphs/test_collect_insert_tweets.py
+++ py_conversations_tests/test_graphs/test_collect_insert_tweets.py
@@ -1,10 +1,16 @@
-from dagster import Any, String, Int
+from dagster import Any
 from py_conversations.graphs.collect_insert_tweets import collect_insert_tweets
 
 
 def test_collect_insert_tweets():
     """
-    TODO: this.*
+    Tests the graph 'collect_insert_tweets'.
+    Ops:
+        get_tweets(topic, last_id, limit) --> insert_items
+    Inputs:
+        topic: topic or phrase with which to query Twitter API
+        last_id: ID of last Tweet in most recent run of this graph to be used to decide starting point for this run's query
+        limit: maximum count of tweets to return
     """
     result = collect_insert_tweets.execute_in_process(
         run_config={
