2fefe6dfc14a63fee3d79dede2ca18bc71faad03
Wes Warriner <fridgehistory@gmail.com>
2022-02-03 18:00:52 -0800

    add to_job config for collect_insert_tweets and rm clean up comments

--- py_conversations/graphs/collect_insert_tweets.py
+++ py_conversations/graphs/collect_insert_tweets.py
@@ -1,15 +1,22 @@
-from dagster import InputDefinition, OutputDefinition, graph, String, Int
+import os
+
+from dagster import graph
 from py_conversations.ops.insert_items import insert_items
 from py_conversations.ops.get_tweets import get_tweets
 
 
 @graph(
     name='collect_insert_tweets',
-    # input_defs=[
-    #     InputDefinition(name='topic', dagster_type=String, default_value='wildfires'),
-    #     InputDefinition(name='last_id', dagster_type=Int, default_value=1485314390639333378),
-    #     InputDefinition(name='limit', dagster_type=Int, default_value=1)
-    # ]
 )
 def collect_insert_tweets(topic, last_id, limit):
-    insert_items(get_tweets(topic, last_id, limit)) # TODO: insert_items takes a list, but we also need to In the last id from the last run and store the last id from this run
+    insert_items(get_tweets(topic, last_id, limit))
+
+collect_insert_tweets = collect_insert_tweets.to_job(
+    config={
+        'inputs': {
+            'topic': { 'value': 'wildfires' },
+            'last_id': { 'value': os.environ.get('LAST_ID')},
+            'limit': {'value': 1 }
+        }
+    }
+)
