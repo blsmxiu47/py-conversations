64b260aa18fdc1be0cf72bbef7ca16793a765de9
Wes Warriner <fridgehistory@gmail.com>
2022-03-27 10:07:40 -0700

    add hourly tweets collection schedule

--- py_conversations/schedules/schedules.py
+++ py_conversations/schedules/schedules.py
@@ -4,7 +4,9 @@ from dagster import schedule
 from dagster.core.definitions import RunRequest
 
 from py_conversations.graphs.collect_insert_googlenews import collect_insert_googlenews
+from py_conversations.graphs.collect_insert_tweets import collect_insert_tweets
 import settings
+
 
 env_vars = {
     'MONGO_USERNAME': settings.MONGO_USERNAME,
@@ -13,7 +15,14 @@ env_vars = {
     'MONGO_DB_NAME': settings.MONGO_DB_NAME
 }
 
+
 @schedule(cron_schedule='0 * * * *', job=collect_insert_googlenews, environment_vars=env_vars, execution_timezone='US/Pacific')
 def hourly_collect_googlenews(_context):
+    now = datetime.now()
+    return RunRequest(run_key=now.strftime('%m/%d/%Y %H:%M:%S'))
+
+
+@schedule(cron_schedule='0 * * * *', job=collect_insert_tweets, environment_vars=env_vars, execution_timezone='US/Pacific')
+def hourly_collect_tweets(_context):
     now = datetime.now()
     return RunRequest(run_key=now.strftime('%m/%d/%Y %H:%M:%S'))
