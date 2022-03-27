324a8481b7008607ba10f1f16299c394e0d06fc2
Wes Warriner <fridgehistory@gmail.com>
2022-03-27 10:04:55 -0700

    update dagster version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setuptools.setup(
     name="py_conversations",
     packages=setuptools.find_packages(exclude=["py_conversations_tests"]),
     install_requires=[
-        "dagster==0.13.10",
-        "dagit==0.13.10",
+        "dagster==0.14.6",
+        "dagit==0.14.6",
         "pytest",
     ],
 )
