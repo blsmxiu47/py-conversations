591854c9ac17ec336ef429d0cc94555eebe47ea5
Wes Warriner <fridgehistory@gmail.com>
2022-03-27 10:07:20 -0700

    make tweets graph formatting consistent with that of googlenews

--- py_conversations/graphs/collect_insert_tweets.py
+++ py_conversations/graphs/collect_insert_tweets.py
@@ -5,11 +5,10 @@ from py_conversations.ops.insert_items import insert_items
 from py_conversations.ops.get_tweets import get_tweets
 
 
-@graph(
-    name='collect_insert_tweets',
-)
+@graph
 def collect_insert_tweets(topic, last_id, limit):
     insert_items(get_tweets(topic, last_id, limit))
+
 
 collect_insert_tweets = collect_insert_tweets.to_job(
     config={
